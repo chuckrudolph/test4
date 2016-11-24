f6040d62b07082ef65ca7a08588b5d616745dfef
chuck rudolph <chuckrudolph@gmail.com>
2016-11-24 08:06:46 -0700

    initial commit

---
+++ ConsoleApplication26/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication26
+{
+    class Program
+    {
+        static int Main(string[] args) {
+<<<<<<< HEAD
+            return 8;
+=======
+            return 5;
+>>>>>>> return 5 with item1
+
+        }
+        bool NewMethod() {
+            return false;
+        }
+    }
+}
