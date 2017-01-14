24db4531b7ae735aa9f459a55b87c5c864615e7d
chuck rudolph <chuckrudolph@gmail.com>
2017-01-14 12:38:48 -0700

    item6 is color black

--- ConsoleApplication26/Program.cs
+++ ConsoleApplication26/Program.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Drawing;
+
 namespace ConsoleApplication26
 {
     class Program
     {
         static int Main(string[] args) {
+
+            Color test = Color.Black;
+
             return 4;
 
         }
